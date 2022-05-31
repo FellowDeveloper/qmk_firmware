e9c32374d5701b6c251ff6f88fbd8ed4e4b7f9bf
Anton Tugolukov <anton.tugolukov@gmail.com>
2022-05-30 22:39:14 -0700

    arrows, mouse and braces

--- keyboards/reviung41/keymaps/siberiangeek/keymap.c
+++ keyboards/reviung41/keymaps/siberiangeek/keymap.c
@@ -9,39 +9,40 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_reviung41(
-    U_NP,              KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,        U_NP,
-    U_NP,              LGUI_T(KC_A),      LALT_T(KC_R),      LCTL_T(KC_S),      LSFT_T(KC_T),      LT(MEDIA, KC_G),   LT(FUN, KC_M),     LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O),   U_NP,
-    U_NP,              KC_Z,              ALGR_T(KC_X),      KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           ALGR_T(KC_DOT),    KC_SLSH,        U_NP,
-                                                                LT(MOUSE, KC_TAB), LT(NAV, KC_ESC),   LT(NUM, KC_SPC),   LT(SYM, KC_ENT),  LT(FUN, KC_BSPC)
+    U_NP,              KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,        KC_CUT,
+    U_NP,              LGUI_T(KC_A),      LALT_T(KC_R),      LCTL_T(KC_S),      LSFT_T(KC_T),      LT(MEDIA, KC_G),   LT(FUN, KC_M),     LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O),   U_PST,
+    U_NP,              KC_Z,              ALGR_T(KC_X),      KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           ALGR_T(KC_DOT),    KC_SLSH,        U_CPY,
+                                                                LT(MOUSE, KC_ESC), LT(NAV, KC_SPC),   LT(NUM, KC_ENT),   LT(SYM, KC_SPC),  LT(FUN, KC_BSPC)
   ),
   [NAV] = LAYOUT_reviung41(
-    U_NP,              RESET,             U_NA,              U_NA,              U_NA,              U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             KC_BSLS,        U_NP,
-    U_NP,              KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_CAPS,           KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,        U_NP,
-    U_NP,              U_NA,              KC_ALGR,           U_NA,              U_NA,              U_NA,              KC_INS,            KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,         U_NP,
-                                                                    KC_TAB,               KC_ESC,            KC_SPC,             KC_ENT,            KC_BSPC
+    U_NP,              RESET,             U_NA,              U_NA,              U_NA,              U_NA,              U_NP,             KC_PGUP,           KC_UP,             KC_HOME,           KC_BSLS,        KC_CUT,
+    U_NP,              KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              U_NP,             KC_LEFT,           KC_DOWN,           KC_RGHT,           U_NP,           U_PST,
+    U_NP,              U_NA,              KC_ALGR,           U_NA,              U_NA,              U_NA,              U_NP,             KC_PGDN,           U_NP,              KC_END,            U_NP,           U_CPY,
+                                                                    KC_ESC,               KC_SPC,            KC_ENT,             KC_SPC,            KC_BSPC
   ),
   [MOUSE] = LAYOUT_reviung41(
-    U_NP,              RESET,             U_NA,              U_NA,              U_NA,              U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             KC_PIPE,        U_NP,
-    U_NP,              KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              U_NU,              KC_MS_L,           KC_MS_D,           KC_MS_U,           KC_MS_R,        U_NP,
+    U_NP,              RESET,             U_NA,              U_NA,              U_NA,              U_NA,              U_RDO,             KC_BTN1,           KC_MS_U,           KC_BTN2,           KC_PIPE,        U_CPY,
+    U_NP,              KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              U_NU,              KC_MS_L,           KC_MS_D,           KC_MS_R,           U_NP,           U_NP,
     U_NP,              U_NA,              KC_ALGR,           U_NA,              U_NA,              U_NA,              U_NU,              KC_WH_L,           KC_WH_D,           KC_WH_U,           KC_WH_R,        U_NP,
-                                                                    KC_TAB,               KC_ESC,            KC_BTN1,          KC_BTN2,           KC_BTN3
+                                                                    KC_ESC,               KC_SPC,            KC_ENT,             KC_SPC,            KC_BSPC
   ),
+  //
   [NUM] = LAYOUT_reviung41(
-    U_NP,            KC_LBRC,             KC_7,              KC_8,               KC_9,              KC_RBRC,           U_NA,               U_NA,               U_NA,            U_NA,               RESET,         U_NP,
-    U_NP,            KC_SCLN,             KC_4,              KC_5,               KC_6,              KC_EQL,            U_NA,               KC_LSFT,            KC_LCTL,         KC_LALT,            KC_LGUI,       U_NP,
-    U_NP,            KC_GRV,              KC_1,              KC_2,               KC_3,              KC_BSLS,           U_NA,               U_NA,               U_NA,            KC_ALGR,            U_NA,          U_NP,
-                                                                    KC_MINS,               KC_0,             KC_SPC,         KC_ENT,           KC_BSPC
+    U_NP,            KC_LBRC,             KC_7,              KC_8,               KC_9,              KC_RBRC,           U_CPY,               KC_MINS,            KC_EQUAL,      KC_UNDS,         KC_PLUS,       U_CPY,
+    U_NP,            LGUI_T(KC_SCLN),     KC_4,              KC_5,               LSFT_T(KC_6),      KC_EQL,            U_PST,               KC_LSFT,            KC_LCTL,       KC_LALT,         KC_LGUI,       U_NP,
+    U_NP,            KC_GRV,              KC_1,              KC_2,               KC_3,              KC_BSLS,           KC_CUT,              KC_LPRN,            KC_RPRN,       KC_TAB,          KC_CAPS,          U_NP,
+                                                                     KC_0,               KC_SPC,            KC_ENT,             KC_SPC,            KC_BSPC
   ), 
   [SYM] = LAYOUT_reviung41(
-    U_NP,            KC_LCBR,             KC_AMPR,          KC_ASTR,            KC_LPRN,           KC_RCBR,           U_NA,               U_NA,               U_NA,            U_NA,               RESET,         U_NP,
+    U_NP,            KC_LCBR,             KC_AMPR,          KC_ASTR,            KC_LPRN,           KC_RCBR,           U_NA,               U_NA,               U_NA,            U_NA,               RESET,         U_CPY,
     U_NP,            KC_COLN,             KC_DLR,           KC_PERC,            KC_CIRC,           KC_PLUS,           U_NA,               KC_LSFT,            KC_LCTL,         KC_LALT,            KC_LGUI,       U_NP,              
     U_NP,            KC_TILD,             KC_EXLM,          KC_AT,              KC_HASH,           KC_PIPE,           U_NA,               U_NA,               U_NA,            KC_ALGR,            U_NA,          U_NP,              
-                                                                    KC_UNDS,               KC_LPRN,          KC_RPRN,           KC_ENT,            KC_BSPC
+                                                                    KC_ESC,               KC_SPC,            KC_ENT,             KC_SPC,            KC_BSPC
   ),
   [FUN] = LAYOUT_reviung41(
-    U_NP,            KC_LPRN,             KC_F7,            KC_F8,              KC_F9,             KC_RPRN,            U_NA,              U_NA,               U_NA,             U_NA,              RESET,         U_NP,
-    U_NP,            KC_F10,              KC_F4,            KC_F5,              KC_F6,             KC_F11,             U_NA,              KC_LSFT,            KC_LCTL,          KC_LALT,           KC_LGUI,       U_NP,
-    U_NP,            KC_F10,              KC_F1,            KC_F2,              KC_F3,             KC_F12,             U_NA,              U_NA,               U_NA,             KC_ALGR,           U_NA,          U_NP,
-                                                                    KC_TAB,               KC_ESC,            KC_SPC,            KC_ENT,            KC_BSPC
+    U_NP,            U_NP,                KC_F7,            KC_F8,              KC_F9,             KC_F10,            U_NA,              U_NA,               U_NA,             U_NA,              RESET,         U_CPY,
+    U_NP,            U_NP,                KC_F4,            KC_F5,              KC_F6,             KC_F11,             U_NA,              KC_LSFT,            KC_LCTL,          KC_LALT,           KC_LGUI,       U_NP,
+    U_NP,            U_NP,                KC_F1,            KC_F2,              KC_F3,             KC_F12,             U_NA,              U_NA,               U_NA,             KC_ALGR,           U_NA,          U_NP,
+                                                                    KC_ESC,               KC_SPC,            KC_ENT,             KC_SPC,            KC_BSPC
   )
 };
--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -11,49 +11,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT_miryoku(
     KC_Q,              KC_W,              KC_F,              KC_P,              KC_B,              KC_J,              KC_L,              KC_U,              KC_Y,              KC_QUOT,
     LGUI_T(KC_A),      LALT_T(KC_R),      LCTL_T(KC_S),      LSFT_T(KC_T),      KC_G,              KC_M,              LSFT_T(KC_N),      LCTL_T(KC_E),      LALT_T(KC_I),      LGUI_T(KC_O),
-    KC_Z,              ALGR_T(KC_X),      KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           ALGR_T(KC_DOT),    KC_SLSH,
-    U_NP,              U_NP,              LT(MEDIA, KC_ESC), LT(NAV, KC_SPC),   LT(MOUSE, KC_TAB), LT(SYM, KC_ENT),   LT(NUM, KC_SPC),  LT(FUN, KC_BSPC),   U_NP,              U_NP
+    KC_Z,              KC_X,              KC_C,              KC_D,              KC_V,              KC_K,              KC_H,              KC_COMM,           KC_DOT,            KC_SLSH,
+    U_NP,              U_NP,              LT(MOUSE, KC_ESC), LT(NAV, KC_SPC),   LT(NUM, KC_ENT),   LT(NUM, KC_ENT),   LT(SYM, KC_SPC),  LT(FUN, KC_BSPC),   U_NP,              U_NP
   ),
   [NAV] = LAYOUT_miryoku(
-    RESET,   U_NA,    U_NA,    U_NA,    U_NA,    U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    KC_CAPS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-    U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    KC_INS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-    U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_ENT,  KC_SPC,  KC_BSPC,  U_NP,    U_NP
+    RESET,   U_NA,    U_NA,    U_NA,    U_NA,    U_RDO,   KC_PGUP, KC_UP,   KC_HOME, U_NP,
+    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, U_NP,
+    U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    KC_INS,  KC_PGDN, KC_PGDN, KC_END,  U_NP,
+    U_NP,    U_NP,    KC_ESC,  KC_SPC,  KC_ENT,  KC_ENT,  KC_SPC,  KC_BSPC, U_NP,    U_NP
   ),
   [MOUSE] = LAYOUT_miryoku(
-    RESET,   U_NA,    U_NA,    U_NA,    U_NA,    U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    U_NU,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,
-    U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NU,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R,
-    U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_BTN1, KC_BTN3, KC_BTN2, U_NP,    U_NP
+    RESET,   U_NA,    U_NA,    U_NA,    U_NA,    KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2, U_NP,
+    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, U_NP,
+    U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NP,    KC_WH_L, KC_BTN3, KC_WH_R, U_NP,
+    U_NP,    U_NP,    KC_ESC,  KC_SPC,  KC_ENT,  KC_ENT,  KC_SPC,  KC_BSPC, U_NP,    U_NP
   ),
   [MEDIA] = LAYOUT_miryoku(
     RESET,   U_NA,    U_NA,    U_NA,    U_NA,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI,
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    U_NU,    KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,
     U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,
-    U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_MSTP, KC_MPLY, KC_MUTE, U_NP,    U_NP
-  ),
-  [MBO] = LAYOUT_miryoku(
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, KC_TRNS, KC_TRNS, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-    U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,   U_RDO,   U_PST,   U_CPY,   U_CUT,   U_UND,
-    U_NP,    U_NP,    KC_BTN2, KC_BTN3, KC_BTN1, KC_BTN1, KC_BTN3, KC_BTN2, U_NP,    U_NP
+    U_NP,    U_NP,    KC_ESC,  KC_SPC,  KC_ENT,  KC_ENT,  KC_SPC,  KC_BSPC, U_NP,    U_NP
   ),
   [NUM] = LAYOUT_miryoku(
-    KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
-    KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,  U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
-    U_NP,    U_NP,    KC_DOT,  KC_0,    KC_MINS, U_NA,    U_NA,    U_NA,    U_NP,    U_NP
+    KC_LBRC, KC_7,    KC_8,    KC_9,         KC_RBRC, U_CPY,   KC_MINS, KC_EQUAL, KC_UNDS, KC_PLUS,
+    KC_SCLN, KC_4,    KC_5,    LSFT_T(KC_6), KC_EQL,  U_PST,   KC_LSFT, KC_LCTL,  KC_LALT, KC_LGUI,
+    KC_GRV,  KC_1,    KC_2,    KC_3,         KC_BSLS, U_CUT,   KC_LPRN, KC_RPRN,  KC_TAB,  KC_CAPS,
+    U_NP,    U_NP,    KC_0,    KC_SPC,       KC_ENT,  KC_ENT,  KC_SPC,  KC_BSPC,  U_NP,    U_NP
   ),
   [SYM] = LAYOUT_miryoku(
-    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
+    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, KC_TAB,  U_NA,    U_NA,    U_NA,    RESET,
     KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS, U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
-    U_NP,    U_NP,    KC_LPRN, KC_RPRN, KC_UNDS, U_NA,    U_NA,    U_NA,    U_NP,    U_NP
+    U_NP,    U_NP,    KC_ESC,  KC_SPC,  KC_ENT,  KC_ENT,  KC_SPC,  KC_BSPC, U_NP,    U_NP
   ),
   [FUN] = LAYOUT_miryoku(
-    KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
-    KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SLCK, U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
-    KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
-    U_NP,    U_NP,    KC_APP,  KC_SPC,  KC_TAB,  U_NA,    U_NA,    U_NA,    U_NP,    U_NP
+    KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_F10, U_NA,    U_NA,    U_NA,    U_NA,    RESET,
+    KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_F11, U_NA,    KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
+    KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_F12, U_NA,    U_NA,    U_NA,    KC_ALGR, U_NA,
+    U_NP,    U_NP,    KC_ESC,  KC_SPC,  KC_ENT,  KC_ENT, KC_SPC,  KC_BSPC, U_NP,    U_NP
   )
 };
